f2d5600b658f776f0ec66f1a029ca0e3ff07128d
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-15 16:00:13 -0400

    display hair color

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ personForm.onsubmit = (e) => {
   e.preventDefault();
   const form = e.target;
   const details = document.querySelector('.details');
+
   const personName = form.personName.value;
   const hairColor = form.hairColor.value;
   const age = form.age.value;
   const birthplace = form.birthplace.value;
-  details.innerHTML = `<em>${personName}</em>`;
+
+  const em = document.createElement('em');
+  em.textContent = personName;
+
+  const colorDiv = document.createElement('div');
+  colorDiv.style.backgroundColor = hairColor;
+  colorDiv.style.height = '50px';
+  colorDiv.style.width = '100px';
+
+  details.appendChild(em);
+  details.appendChild(colorDiv);
 }
