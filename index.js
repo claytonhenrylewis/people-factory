56a54619267ec42098f5e220413a79361f43593c
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-15 16:16:16 -0400

    Add all form values to details

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ personForm.onsubmit = (e) => {
   const age = form.age.value;
   const birthplace = form.birthplace.value;
 
-  const em = document.createElement('em');
-  em.textContent = personName;
+  /*const list = document.createElement('li');
+
+  const em = document.createElement('li');
+  em.innerHTML = `Name: ${personName}`;
+
+  const ageDiv = document.createElement('li');
+  ageDiv.textContent = age;
+
+  const placeDiv = document.createElement('li');
+  placeDiv.textContent = birthplace;
+  */
 
   const colorDiv = document.createElement('div');
   colorDiv.style.backgroundColor = hairColor;
   colorDiv.style.height = '50px';
   colorDiv.style.width = '100px';
 
-  details.appendChild(em);
-  details.appendChild(colorDiv);
+  details.innerHTML = `
+  <ul>
+    <li>Name: ${personName}</li>
+    <li>Hair Color: ${colorDiv.outerHTML}</li>
+    <li>Age: ${age}</li>
+    <li>Birthplace: ${birthplace}</li>
+  </ul>`;
 }
