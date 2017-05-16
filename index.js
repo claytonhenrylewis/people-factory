cdc28467af1ec9ebe3e55ab070dc8eebe80104c5
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-16 14:44:35 -0400

    IIFE

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-const personForm = document.querySelector('form');
+(function () {
+  const personForm = document.querySelector('form');
 
-const renderColor = (name, value) => {
-  const li = document.createElement('li');
-  li.textContent = `${name}:`;
-  const colorDiv = document.createElement('div');
-  colorDiv.style.backgroundColor = value;
-  colorDiv.style.height = '50px';
-  colorDiv.style.width = '100px';
-  li.appendChild(colorDiv);
-  return li;
-}
-
-const renderText = (name, value) => {
-  const li = document.createElement('li');
-  li.innerHTML = `${name}: ${value}`;
-  return li;
-}
-
-/*
-const renderList = (inputs, labels) => {
-  const list = document.createElement('ul');
-  for (let i = 0; i < inputs.length; i++) {
-    list.appendChild(handleInput(inputs[i], labels[i]))
+  const renderColor = (value) => {
+    const colorDiv = document.createElement('div');
+    colorDiv.style.backgroundColor = value;
+    colorDiv.style.height = '50px';
+    colorDiv.style.width = '100px';
+    return colorDiv;
   }
-  return list;
-}
-*/
 
-const renderList = (person) => {
-  const list = document.createElement('ul');
-  Array.from(person).map((input, i, formElements) => {
-    if (input.value) {
-      const li = renderText(input.name, input.value);
-      list.appendChild(li);
+  const renderText = (name, value) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${name}: ${value}`;
+    return li;
+  }
+
+  /*
+  const renderList = (inputs, labels) => {
+    const list = document.createElement('ul');
+    for (let i = 0; i < inputs.length; i++) {
+      list.appendChild(handleInput(inputs[i], labels[i]))
     }
-  });
-  return list;
-}
-
-const handleInput = (input, label) => {
-  if (input.type === 'color') {
-    return renderColor(label.textContent, input.value);
+    return list;
   }
-  return renderText(label.textContent, input.value);
-}
+  */
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const details = document.querySelector('.details');
-  const inputs = form.querySelectorAll('input');
-  const labels = form.querySelectorAll('label');
+  const renderList = (person) => {
+    const list = document.createElement('ul');
+    Array.from(person).map((input, i, formElements) => {
+      if (input.value) {
+        let value = input.value;
+        if (input.type === 'color') {
+          value = renderColor(input.value).outerHTML;
+        }
+        const li = renderText(input.name, value);
+        list.appendChild(li);
+      }
+    });
+    return list;
+  }
 
-  const list = renderList(form.elements);
-  details.appendChild(list);
-}
+  const handleInput = (input, label) => {
+    if (input.type === 'color') {
+      return renderColor(label.textContent, input.value);
+    }
+    return renderText(label.textContent, input.value);
+  }
 
-personForm.addEventListener('submit', handleSubmit);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const details = document.querySelector('.details');
+    const inputs = form.querySelectorAll('input');
+    const labels = form.querySelectorAll('label');
+
+    const list = renderList(form.elements);
+    details.appendChild(list);
+  }
+
+  personForm.addEventListener('submit', handleSubmit);
+})();
