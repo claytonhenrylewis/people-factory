1e07acd61e326287b6279daec131c92ef94c133b
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-16 09:48:45 -0400

    Use appendChild and createElement

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 const personForm = document.querySelector('form');
 
-const renderColor = (hairColor) => {
+const renderColor = (name, value) => {
+  const li = document.createElement('li');
+  li.textContent = name + ':';
   const colorDiv = document.createElement('div');
-  colorDiv.style.backgroundColor = hairColor;
+  colorDiv.style.backgroundColor = value;
   colorDiv.style.height = '50px';
   colorDiv.style.width = '100px';
-  return colorDiv;
+  li.appendChild(colorDiv);
+  return li;
+}
+
+const renderText = (name, value) => {
+  const li = document.createElement('li');
+  li.textContent = name + ': ' + value;
+  return li;
+}
+
+const handleInput = (label) => {
+  inputs = document.getElementsByName(label['for']);
+  console.log(label.attributes);
 }
 
 const handleSubmit = (e) => {
   e.preventDefault();
   const form = e.target;
   const details = document.querySelector('.details');
+  const inputs = document.querySelectorAll('input');
+  const labels = document.querySelectorAll('label');
+  labels.forEach(handleInput);
 
   const personName = form.personName.value;
   const hairColor = form.hairColor.value;
   const age = form.age.value;
   const birthplace = form.birthplace.value;
 
-  const colorDiv = renderColor(hairColor);
+  const personDiv = renderText("Name", personName);
+  const colorDiv = renderColor("Hair Color", hairColor);
+  const ageDiv = renderText("Age", age);
+  const placeDiv = renderText("Birthplace", birthplace);
 
+  const list = document.createElement('ul');
+  list.appendChild(personDiv);
+  list.appendChild(colorDiv);
+  list.appendChild(ageDiv);
+  list.appendChild(placeDiv);
+
+  details.appendChild(list);
+  /*
   details.innerHTML = `
   <ul>
     <li>Name: ${personName}</li>
@@ -27,7 +55,7 @@ const handleSubmit = (e) => {
     <li>Age: ${age}</li>
     <li>Birthplace: ${birthplace}</li>
   </ul>`;
+  */
 }
 
-//personForm.onsubmit = handleSubmit;
 personForm.addEventListener('submit', handleSubmit);
