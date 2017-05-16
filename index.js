0f316156114899fe995d619d939e736328f2ef1b
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-16 15:20:24 -0400

    Wrap everything in object

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-{
-  const personForm = document.querySelector('form');
+const App = {
+  init() {
+    const personForm = document.querySelector('form');
+    personForm.addEventListener('submit', (e) => this.handleSubmit(e));
+  },
 
-  const renderColor = (value) => {
+  renderColor(value) {
     const colorDiv = document.createElement('div');
     colorDiv.style.backgroundColor = value;
     colorDiv.style.height = '50px';
     colorDiv.style.width = '100px';
     return colorDiv;
-  }
+  },
 
-  const renderText = (name, value) => {
+  renderText(name, value) {
     const li = document.createElement('li');
     li.innerHTML = `${name}: ${value}`;
     return li;
-  }
+  },
 
   /*
   const renderList = (inputs, labels) => {
@@ -25,38 +28,38 @@
   }
   */
 
-  const renderList = (person) => {
+  renderList(person) {
     const list = document.createElement('ul');
     Array.from(person).map((input, i, formElements) => {
       if (input.value) {
         let value = input.value;
         if (input.type === 'color') {
-          value = renderColor(input.value).outerHTML;
+          value = this.renderColor(input.value).outerHTML;
         }
-        const li = renderText(input.name, value);
+        const li = this.renderText(input.name, value);
         list.appendChild(li);
       }
     });
     return list;
-  }
+  },
 
-  const handleInput = (input, label) => {
+  handleInput(input, label) {
     if (input.type === 'color') {
-      return renderColor(label.textContent, input.value);
+      return this.renderColor(label.textContent, input.value);
     }
-    return renderText(label.textContent, input.value);
-  }
+    return this.renderText(label.textContent, input.value);
+  },
 
-  const handleSubmit = (e) => {
+  handleSubmit(e) {
     e.preventDefault();
     const form = e.target;
     const details = document.querySelector('.details');
     const inputs = form.querySelectorAll('input');
     const labels = form.querySelectorAll('label');
 
-    const list = renderList(form.elements);
+    const list = this.renderList(form.elements);
     details.appendChild(list);
-  }
-
-  personForm.addEventListener('submit', handleSubmit);
+  },
 }
+
+App.init();
